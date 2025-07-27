7aabeec9c5827dcb3c72c6b338e5bac74fc3c407
Fred Zabulon <fredzabulon133@gmail.com>
2025-07-28 00:07:21 +0300

    defined the listings view

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import listing
 # Create your views here.
+
+def listing_list(request):
+    listings = listing.objects.all()
+    context = {
+        "listings": listings
+    }
+    return render(request, "index.html", context)
